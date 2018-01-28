04555d70cf1e08d15afa7f69373b9f25c5ad1814
QQduan <377028119@qq.com>
2018-01-28 22:35:33 +0800

    commit

--- FastCat/src/main/java/com/qqduan/FastCat/core/ApplicationContext.java
+++ FastCat/src/main/java/com/qqduan/FastCat/core/ApplicationContext.java
@@ -63,13 +63,13 @@ public class ApplicationContext {
 			HashMap<String, Element> properties = getProperties(clz);
 			Field[] fields = clz.getDeclaredFields();
 			for (Field field : fields) {
-				if(field.isAnnotationPresent(AutoCycleLife.class)){
-					if(properties==null){
+				if (field.isAnnotationPresent(AutoCycleLife.class)) {
+					if (properties == null) {
 						throw new ContextXmlError(clz.getName() + " is not found in context xml");
 					}
 					field.setAccessible(true);
 					Element propertyEly = properties.get(field.getName());
-					setField(field,newInstance,propertyEly);
+					setField(field, newInstance, propertyEly);
 				}
 			}
 		} catch (InstantiationException | IllegalAccessException e) {
@@ -78,8 +78,58 @@ public class ApplicationContext {
 		return null;
 	}
 
-	private <T> void setField(Field field, T newInstance, Element propertyEly) {
-		
+	private <T> void setField(Field field, T newInstance, Element propertyEle) {
+		if (propertyEle.attribute("value") != null) {
+			setValue(field, newInstance, propertyEle);
+		} else if (propertyEle.attribute("ref") != null) {
+			setRef(field, newInstance, propertyEle);
+		}
+	}
+
+	private <T> void setRef(Field field, T newInstance, Element propertyEle) {
+		String className = propertyEle.attributeValue("ref");
+		try {
+			Class<?> clz = (Class<?>) Class.forName(className);
+			field.set(newInstance, getBean(clz));
+		} catch (ClassNotFoundException | IllegalArgumentException | IllegalAccessException e) {
+			throw new ContextXmlError(className + " is not found");
+		}
+	}
+
+	public Object getBean(Class<?> clz) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private <T> void setValue(Field field, T newInstance, Element propertyEle) {
+		String value = propertyEle.attributeValue("value");
+		try {
+			Class<?> type = field.getType();
+			if (type.isAssignableFrom(Integer.class) || type.isAssignableFrom(int.class)) {
+				field.set(newInstance, Integer.valueOf(value));
+			} else if (type.isAssignableFrom(Character.class) || type.isAssignableFrom(char.class)) {
+				if (value.length() != 1) {
+					throw new ContextXmlError(field.getName() + " is not a character");
+				}
+				field.set(newInstance, value.charAt(0));
+			} else if (type.isAssignableFrom(Boolean.class) || type.isAssignableFrom(boolean.class)) {
+				field.set(newInstance, Boolean.valueOf(value));
+			} else if (type.isAssignableFrom(Long.class) || type.isAssignableFrom(long.class)) {
+				field.set(newInstance, Long.valueOf(value));
+			} else if (type.isAssignableFrom(Short.class) || type.isAssignableFrom(short.class)) {
+				field.set(newInstance, Short.valueOf(value));
+			} else if (type.isAssignableFrom(Float.class) || type.isAssignableFrom(float.class)) {
+				field.set(newInstance, Float.valueOf(value));
+			} else if (type.isAssignableFrom(Double.class) || type.isAssignableFrom(double.class)) {
+				field.set(newInstance, Double.valueOf(value));
+			} else if (type.isAssignableFrom(String.class)) {
+				field.set(newInstance, value);
+			} else {
+				throw new ContextXmlError("type of " + field.getName() + " is not support");
+			}
+		} catch (IllegalArgumentException | IllegalAccessException e) {
+			e.printStackTrace();
+		}
 	}
 
 	private <T> HashMap<String, Element> getProperties(Class<T> clz) {
